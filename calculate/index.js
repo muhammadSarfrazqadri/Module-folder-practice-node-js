011648ddb0805d9a8234112e90903e5b10f0fb52
Muhammad Sarfraz <msowner.best@gmail.com>
2025-08-06 12:47:18 +0500

    Create index.js

---
+++ calculate/index.js
@@ -0,0 +1,10 @@
+const add = require('.add.js');
+const subtract = require('.subtract.js');
+const multiply = require('.multiply.js');
+const divide = require('.divide.js');
+module.exports = {
+  add,
+  subtract,
+  divide,
+  multiply
+}
